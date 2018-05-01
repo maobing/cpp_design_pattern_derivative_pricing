dda783d8dbbe93e78b624d683fd48ebf14dd961a
Mao Ye <maoye@Maos-Air.fios-router.home>
2018-04-30 22:48:41 -0400

    Add ch2 page 15

---
+++ PayOff1.cpp
@@ -0,0 +1,18 @@
+#include<PayOff1.h>
+#include<MinMax.h>
+
+PayOff::PayOff(double Strike_, OptionType TheOptionsType_)
+    : Strike(Strike_), TheOptionsType(TheOptionsType_)
+{}
+
+double PayOff::operator()(double spot) const {
+    switch (TheOptionsType_)
+    {
+        case call : 
+            return max(spot - Strike, 0.0);
+        case put:
+            return max(Strike - spot, 0.0);
+        default:
+            throw("unknow option type found");
+    }
+}
