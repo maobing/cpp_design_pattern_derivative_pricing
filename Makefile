662d408c4c8c0dcfb4da74ead9abd66adae0de3b
Mao Ye <maoye@Maos-Air.fios-router.home>
2018-05-01 22:51:31 -0400

    Finish ch2

---
+++ Makefile
@@ -0,0 +1,3 @@
+SimpleMCMain2: SimpleMCMain2.cpp SimpleMC.cpp Random1.cpp PayOff1.cpp
+	g++ -Wall -o SimpleMCMain2 SimpleMCMain2.cpp SimpleMC.cpp Random1.cpp PayOff1.cpp
+
--- PayOff1.cpp
+++ PayOff1.cpp
@@ -1,12 +1,14 @@
-#include<PayOff1.h>
-#include<MinMax.h>
+#include "PayOff1.h"
+#include <algorithm>
+
+using namespace std;
 
 PayOff::PayOff(double Strike_, OptionType TheOptionsType_)
     : Strike(Strike_), TheOptionsType(TheOptionsType_)
 {}
 
 double PayOff::operator()(double spot) const {
-    switch (TheOptionsType_)
+    switch (TheOptionsType)
     {
         case call : 
             return max(spot - Strike, 0.0);
